03d2e7c384d159dfe938da6692bacaa9c96049eb
Jhonatan Amórtegui <Jhonatan Amrtegui>
2018-10-02 00:01:41 -0500

    Agregando archivos sql al proyecto.

---
+++ data/EsquemaSuperAndes.sql
@@ -0,0 +1,3 @@
+--- Sentencias SQL para la creación del esquema de parranderos
+--- Las tablas tienen prefijo A_ para facilitar su acceso desde SQL Developer
+
---
+++ data/LimpiezaSuperAndes.sql
@@ -0,0 +1,12 @@
+--- Sentencias SQL para la creación del esquema de parranderos
+--- Las tablas tienen prefijo A_ para facilitar su acceso desde SQL Developer
+
+-- USO
+-- Copie el contenido deseado de este archivo en una pestaña SQL de SQL Developer
+-- Ejecútelo como un script - Utilice el botón correspondiente de la pestaña utilizada
+    
+-- Eliminar todas las tablas de la base de datos
+
+
+-- Eliminar el contenido de todas las tablas de la base de datos
+-- El orden es importante. Por qué?
